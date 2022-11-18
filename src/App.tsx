d27180c1079bbffab15d5d7a7c0ea3f4ac4e65e3
belln-eng <jhend1204@gmail.com>
2022-11-18 20:16:42 +0900

    [Mod] display test

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,14 @@ const theme = extendTheme({ colors });
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Global styles={SetCss} />
+
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path={RoutingPath.cssInit} element={<CssInit />} />
         </Routes>
       </BrowserRouter>
-    </ChakraProvider>
+
   );
 }
 
